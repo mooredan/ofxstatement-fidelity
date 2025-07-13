1c7ff9acc7e1958e258e132f5ac11825395ccd7d
Dan Moore <mooredan@suncup.net>
2025-07-13 22:45:55 +0700

    Basic test now in place

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -8,7 +8,7 @@ from ofxstatement_fidelity.plugin import FidelityPlugin
 def test_fidelity() -> None:
     plugin = FidelityPlugin(UI(), {})
     here = os.path.dirname(__file__)
-    fidelity_filename = os.path.join(here, "fidelity-statement.csv")
+    fidelity_filename = os.path.join(here, "History_for_Account_2TB000009.csv")
 
     parser = plugin.get_parser(fidelity_filename)
     statement = parser.parse()
